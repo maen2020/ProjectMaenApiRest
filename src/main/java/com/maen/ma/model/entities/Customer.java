d7869434406b4cd09a32385c92cc1052344b9890
maen210212 <maen210212@gmail.com>
2024-07-16 15:19:33 -0600

    Creation of the customer entity.

---
+++ src/main/java/com/maen/ma/model/entities/Customer.java
@@ -0,0 +1,29 @@
+package com.maen.ma.model.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+//Entidad: Objeto que contiene atributos particulares.
+@Getter //Getter de los atributos.
+@Setter //Setter de los atributos.
+@Builder
+@AllArgsConstructor //Constructor con parametros.
+@NoArgsConstructor //Constructor sin parametros.
+@Entity //Definir que es una entidad(Objecto).
+@Table(name = "Customer") //Mapeo hacia la tabla de la DB.
+public class Customer {
+
+    //Atributos.
+    @Id
+    @Column(name = "id_customer")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(name = "last_name")
+    private String lastName;
+    private String email;
+    @Column(name = "registration_date")
+    private Date registrationDate;
+}
